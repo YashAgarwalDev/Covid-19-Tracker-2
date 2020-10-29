258a2af053ee98caecbbd3180a0d9f2508c4b788
Yash Agarwal <yashsidd98@gmail.com>
2020-10-29 16:30:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from '@material-ui/core';
+import InfoBox from './components/InfoBox/InfoBox';
+import LineGraph from './components/LineGraph/LineGraph';
+import Table from './components/Table/Table';
+import Map from './components/Map/Map';
+import { sortData, prettyPrintStat } from './util';
+import 'leaflet/dist/leaflet.css';
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+  // map props
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  // circles for the map
+  const [mapCountries, setMapCountries] = useState([]);
+  // casesType can be 'cases, recovered, deaths'
+  const [casesType, setCasesType] = useState('cases');
+
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all')
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2, // us, usa, fr
+          }));
+
+          const sortedData = sortData(data); // sort table
+          setCountries(countries); // change the countries
+          setMapCountries(data); // get all the json response for all the countries
+          setTableData(sortedData);
+        });
+    };
+
+    getCountriesData(); // call useEffect
+  }, []);
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+
+    const url =
+      countryCode === 'worldwide'
+        ? 'https://disease.sh/v3/covid-19/all'
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url).then((res) => res.json()).then((data) => {
+      setCountry(countryCode);
+      // all of the data from the country response
+      setCountryInfo(data);
+      // change the map according to chosen country from dropdown
+      if (countryCode === 'worldwide') {
+        setMapCenter([34.80746, -40.4796]);
+        setMapZoom(2);
+      } else {
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
+      }
+    });
+  };
+
+  // console.log(countryInfo);
+
+  return (
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>Covid-19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            {/* worldwide selected */}
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {/* Loop through all the countries and show a dropdown list of the options  - short term memory for state */}
+              {countries.map((country) => (
+                <MenuItem value={country.value} key={country.value}>
+                  {country.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox
+            isRed
+            active={casesType === 'cases'}
+            onClick={(e) => setCasesType('cases')}
+            title="Coronavirus cases"
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={countryInfo.cases}
+            Total
+          />
+          <InfoBox
+            active={casesType === 'recovered'}
+            onClick={(e) => setCasesType('recovered')}
+            title="Recovered"
+            cases={prettyPrintStat(countryInfo.todayRecovered)}
+            total={countryInfo.recovered}
+            Total
+          />
+          <InfoBox
+            isRed
+            active={casesType === 'deaths'}
+            onClick={(e) => setCasesType('deaths')}
+            title="Deaths"
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={countryInfo.deaths}
+            Total
+          />
+        </div>
+        <Map
+          countries={mapCountries}
+          center={mapCenter}
+          zoom={mapZoom}
+          // change the colors with case types
+          casesType={casesType}
+        />
+      </div>
+
+      <Card className="app__right">
+        <CardContent>
+          {/* <div className="app__information"> */}
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+          <h3 className="app__graphTitle">Worldwide new {casesType}</h3>
+          <LineGraph casesType={casesType} className="app__graph" />
+          {/* </div> */}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/InfoBox/InfoBox.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './InfoBox.css';
+import { Card, CardContent, Typography } from '@material-ui/core';
+
+// onClick - custom prop
+function InfoBox({ title, cases, isRed, active, total, ...props }) {
+  return (
+    <Card
+      onClick={props.onClick}
+      // add infoBox--selected class if it is active
+      className={`infoBox ${active && 'infoBox--selected'} ${isRed &&
+        'infoBox--red'}`}>
+      <CardContent>
+        {/* Title */}
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
+
+        {/* +120K number of cases */}
+        <h2 className={`infoBox__cases ${!isRed && 'infoBox__cases--green'}`}>
+          {cases}
+        </h2>
+
+        {/* 1.2M Total */}
+        <Typography className="infoBox__total" color="textSecondary">
+          {total}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
